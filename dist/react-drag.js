c9bf354571f3fe989b71b541c9681f6b60086737
mgechev <mgechev@gmail.com>
2015-09-08 09:04:04 +0300

    Handle classSet warning

--- dist/react-drag.js
+++ dist/react-drag.js
@@ -11,8 +11,29 @@
 /* exported ReactDrag */
 'use strict';
 
+function classNames() {
+  var classes = '';
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+    if (!arg) continue;
+    var argType = typeof arg;
+    if ('string' === argType || 'number' === argType) {
+      classes += ' ' + arg;
+    } else if (Array.isArray(arg)) {
+      classes += ' ' + classNames.apply(null, arg);
+    } else if ('object' === argType) {
+      for (var key in arg) {
+        if (arg.hasOwnProperty(key) && arg[key]) {
+          classes += ' ' + key;
+        }
+      }
+    }
+  }
+  return classes.substr(1);
+}
+
 var emptyFunction = function () {};
-var CX = React.addons.classSet;
+var CX = classNames;
 
 function createUIEvent(draggable) {
   return {
